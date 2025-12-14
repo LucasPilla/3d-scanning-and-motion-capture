e88aa996a8f9c652e8fe7bafadb97ac87a4b4f6a
LucasPilla <elucaspimentel@gmail.com>
2025-12-14 16:32:15 +0100

    added comment with BODY_25 joint order

--- src/PoseDetector.cpp
+++ src/PoseDetector.cpp
@@ -90,7 +90,34 @@ Pose2D PoseDetector::detect(const cv::Mat& frame)
     }
 
     // Return Pose2D.
-    // Joints remain in OpenPose BODY_25 order; we’ll align this with SMPL later
+    // Joints remain in OpenPose BODY_25 order:
+    // 0 -> Nose
+    // 1 -> Neck
+    // 2 -> RShoulder
+    // 3 -> RElbow
+    // 4 -> RWrist
+    // 5 -> LShoulder
+    // 6 -> LElbow
+    // 7 -> LWrist
+    // 8 -> MidHip
+    // 9 -> RHip
+    // 10 -> RKnee
+    // 11 -> RAnkle
+    // 12 -> LHip
+    // 13 -> LKnee
+    // 14 -> LAnkle
+    // 15 -> REye
+    // 16 -> LEye
+    // 17 -> REar
+    // 18 -> LEar
+    // 19 -> LBigToe
+    // 20 -> LSmallToe
+    // 21 -> LHeel
+    // 22 -> RBigToe
+    // 23 -> RSmallToe
+    // 24 -> RHeel
+    // 25 -> Background
+
     pose2D.keypoints = keypoints;
 
     return pose2D;
