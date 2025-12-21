baeeedacfc54f6b57192d838310ba838d36420a2
OzdenGursoy <ogursoy15@ku.edu.tr>
2025-12-21 04:22:01 +0100

    For OpenPose Preprocessing GPU

---
+++ build_gpu/CMakeFiles/pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pipeline.dir/src/FittingOptimizer.cpp.o"
+  "CMakeFiles/pipeline.dir/src/FittingOptimizer.cpp.o.d"
+  "CMakeFiles/pipeline.dir/src/PoseDetector.cpp.o"
+  "CMakeFiles/pipeline.dir/src/PoseDetector.cpp.o.d"
+  "CMakeFiles/pipeline.dir/src/SMPLModel.cpp.o"
+  "CMakeFiles/pipeline.dir/src/SMPLModel.cpp.o.d"
+  "CMakeFiles/pipeline.dir/src/TemporalSmoother.cpp.o"
+  "CMakeFiles/pipeline.dir/src/TemporalSmoother.cpp.o.d"
+  "CMakeFiles/pipeline.dir/src/VideoLoader.cpp.o"
+  "CMakeFiles/pipeline.dir/src/VideoLoader.cpp.o.d"
+  "CMakeFiles/pipeline.dir/src/Visualization.cpp.o"
+  "CMakeFiles/pipeline.dir/src/Visualization.cpp.o.d"
+  "CMakeFiles/pipeline.dir/src/main.cpp.o"
+  "CMakeFiles/pipeline.dir/src/main.cpp.o.d"
+  "pipeline"
+  "pipeline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
