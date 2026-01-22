2be67de6fdf8ab6ef3ffc37dabb6aae0b25982ff
LucasPilla <elucaspimentel@gmail.com>
2026-01-22 18:19:32 +0100

    added optional prior data to SMPLModel (gmm and capsules regressor)

--- src/SMPLModel.cpp
+++ src/SMPLModel.cpp
@@ -209,6 +209,100 @@ bool SMPLModel::loadFromJson(const std::string &jsonPath)
 				weights_(v, jIdx) = static_cast<double>(w[v][jIdx]);
 			}
 		}
+
+        // -------- gmm_means --------
+        if (j.contains("gmm_means"))
+        {
+            const auto &gmm_means = j.at("gmm_means");
+            int gmmMeansRows = static_cast<int>(gmm_means.size());
+            int gmmMeansCols = static_cast<int>(gmm_means[0].size());
+            gmmMeans_.resize(gmmMeansRows, gmmMeansCols);
+            for (int i = 0; i < gmmMeansRows; ++i)
+                for (int k = 0; k < gmmMeansCols; ++k)
+                    gmmMeans_(i, k) = static_cast<double>(gmm_means[i][k]);
+        }
+
+        // -------- gmm_covars --------
+        // JSON is 3D (8, 69, 69). We flatten the last two dimensions to fit Eigen Matrix (8, 4761).
+        if (j.contains("gmm_covars"))
+        {
+            const auto &gmm_covars = j.at("gmm_covars");
+            int gmmCovarsRows = static_cast<int>(gmm_covars.size());
+            int gmmCovarsDim1 = static_cast<int>(gmm_covars[0].size());
+            int gmmCovarsDim2 = static_cast<int>(gmm_covars[0][0].size());
+            
+            gmmCovars_.resize(gmmCovarsRows, gmmCovarsDim1 * gmmCovarsDim2);
+            
+            for (int i = 0; i < gmmCovarsRows; ++i)
+            {
+                for (int r = 0; r < gmmCovarsDim1; ++r)
+                {
+                    for (int c = 0; c < gmmCovarsDim2; ++c)
+                    {
+                        int flatIdx = r * gmmCovarsDim2 + c;
+                        gmmCovars_(i, flatIdx) = static_cast<double>(gmm_covars[i][r][c]);
+                    }
+                }
+            }
+        }
+
+        // -------- gmm_weights --------
+        if (j.contains("gmm_weights"))
+        {
+            const auto &gmm_weights = j.at("gmm_weights");
+            int gmmWeightsRows = static_cast<int>(gmm_weights.size());
+            gmmWeights_.resize(gmmWeightsRows, 1);
+            for (int i = 0; i < gmmWeightsRows; ++i)
+                gmmWeights_(i, 0) = static_cast<double>(gmm_weights[i]);
+        }
+
+        // -------- capsule_v2lens --------
+        if (j.contains("capsule_v2lens"))
+        {
+            const auto &capsule_v2lens = j.at("capsule_v2lens");
+            int capsuleV2lensRows = static_cast<int>(capsule_v2lens.size());
+            int capsuleV2lensCols = static_cast<int>(capsule_v2lens[0].size());
+            capsuleV2Lens_.resize(capsuleV2lensRows, capsuleV2lensCols);
+            for (int i = 0; i < capsuleV2lensRows; ++i)
+                for (int k = 0; k < capsuleV2lensCols; ++k)
+                    capsuleV2Lens_(i, k) = static_cast<double>(capsule_v2lens[i][k]);
+        }
+
+        // -------- capsule_betas2lens --------
+        if (j.contains("capsule_betas2lens"))
+        {
+            const auto &capsule_betas2lens = j.at("capsule_betas2lens");
+            int capsuleBetas2lensRows = static_cast<int>(capsule_betas2lens.size());
+            int capsuleBetas2lensCols = static_cast<int>(capsule_betas2lens[0].size());
+            capsuleBetas2Lens_.resize(capsuleBetas2lensRows, capsuleBetas2lensCols);
+            for (int i = 0; i < capsuleBetas2lensRows; ++i)
+                for (int k = 0; k < capsuleBetas2lensCols; ++k)
+                    capsuleBetas2Lens_(i, k) = static_cast<double>(capsule_betas2lens[i][k]);
+        }
+
+        // -------- capsule_v2rads --------
+        if (j.contains("capsule_v2rads"))
+        {
+            const auto &capsule_v2rads = j.at("capsule_v2rads");
+            int capsuleV2radsRows = static_cast<int>(capsule_v2rads.size());
+            int capsuleV2radsCols = static_cast<int>(capsule_v2rads[0].size());
+            capsuleV2Rads_.resize(capsuleV2radsRows, capsuleV2radsCols);
+            for (int i = 0; i < capsuleV2radsRows; ++i)
+                for (int k = 0; k < capsuleV2radsCols; ++k)
+                    capsuleV2Rads_(i, k) = static_cast<double>(capsule_v2rads[i][k]);
+        }
+
+        // -------- capsule_betas2rads --------
+        if (j.contains("capsule_betas2rads"))
+        {
+            const auto &capsule_betas2rads = j.at("capsule_betas2rads");
+            int capsuleBetas2radsRows = static_cast<int>(capsule_betas2rads.size());
+            int capsuleBetas2radsCols = static_cast<int>(capsule_betas2rads[0].size());
+            capsuleBetas2Rads_.resize(capsuleBetas2radsRows, capsuleBetas2radsCols);
+            for (int i = 0; i < capsuleBetas2radsRows; ++i)
+                for (int k = 0; k < capsuleBetas2radsCols; ++k)
+                    capsuleBetas2Rads_(i, k) = static_cast<double>(capsule_betas2rads[i][k]);
+        }
 	}
 	catch (const std::exception &e)
 	{
